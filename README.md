775000e5056b854420ffb40f60660ca009f03f1a
Leo dev <klesti.selimay@icloud.com>
2025-10-28 19:53:56 +0100

    Simple emulation (not even close)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # forge
 A flash emulator with my own stuff
+
+## Opcodes
+
+| OP | INST | Action |
+|-----------|-----------|-----------|
+| 0 | LBK | Goes to the next instruction |
+| 1 | HLT | Stops further emulation of the program |
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,33 @@ mod binary;
 mod emulator;
 mod text;
 
+use std::sync::Arc;
+
+use osui::prelude::*;
+
 fn main() {
-    
+    let screen = Screen::new();
+
+    app(&screen).draw(&screen);
+
+    screen.run().unwrap();
+}
+
+fn app(_screen: &Arc<Screen>) -> Rsx {
+    let pc = use_state::<u32>(0);
+
+    // Spawn a thread to emulate the program
+    std::thread::spawn({
+        let pc = pc.clone();
+        move || loop {
+            **pc.get() += 1;
+        }
+    });
+
+    rsx! {
+        FlexRow {
+            %pc
+            "Program Counter: {pc}"
+        }
+    }
 }
